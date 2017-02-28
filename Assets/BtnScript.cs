1068ba79e05f00ba90763dd46a72438ca6ed1021
Saurabh Pathak <pathaks@tcd.ie>
2017-02-28 22:49:04 +0000

    Setting up environment and adding moving model

---
+++ Assets/BtnScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class BtnScript : MonoBehaviour, IVirtualButtonEventHandler {
+	private GameObject btnObject;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+		btnObject = GameObject.Find ("playButton");
+		zombie = GameObject.Find ("zombie");
+		btnObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb) {
+		zombie.GetComponent<Animation> ().Play ();
+		Debug.Log ("Button Pressed!!!");
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
+		zombie.GetComponent<Animation> ().Stop ();
+	}
+}
