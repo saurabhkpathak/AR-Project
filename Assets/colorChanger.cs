48d916115b1c97bfede6dd637893b713f0620bae
Saurabh Pathak <pathaks@tcd.ie>
2017-03-25 21:01:31 +0000

    adding code for applying color to car

---
+++ Assets/colorChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChanger : MonoBehaviour {
+	private GameObject carChasis;
+	public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+		carChasis = GameObject.Find ("Chassis");
+		carChasis.GetComponent<Renderer>().sharedMaterial = materials[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
