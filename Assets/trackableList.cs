8285505640573c2cf501b2cc0b2b4558ec0c8136
Saurabh Pathak <pathaks@tcd.ie>
2017-04-29 23:01:00 +0100

    fixing the functionality to make the car go to new marker and go back to previous marker

--- Assets/trackableList.cs
+++ Assets/trackableList.cs
@@ -13,6 +13,13 @@ public class trackableList : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		int i = 0;
+		int indexFoundAt = 0;
+		bool found = false;
+		TrackableItem[] trackableItems = new TrackableItem[2];
+		for (int k =0; k < trackableItems.Length; k++) {
+			trackableItems [k] = new TrackableItem ();
+		}
 		// Get the Vuforia StateManager
 		StateManager sm = TrackerManager.Instance.GetStateManager ();
 
@@ -23,9 +30,30 @@ public class trackableList : MonoBehaviour {
 
 		// Iterate through the list of active trackables
 		foreach (TrackableBehaviour tb in activeTrackables) {
-			if (tb.TrackableName == "notebook2") {
-				mainCar.GetComponent<Transform> ().position = tb.transform.position;
+			Debug.Log ("index at: " + i);
+			trackableItems [i].name = tb.TrackableName;
+			trackableItems [i].position = tb.transform.position;
+			i++;
+		}
+
+		if (trackableItems.Length != 0) {
+			for (int j = 0; j < trackableItems.Length; j++) {
+				if (trackableItems [j].name == "notebook2") {
+					found = true;
+					indexFoundAt = j;
+				}
+			}
+			if (found) {
+				mainCar.GetComponent<Transform> ().position = trackableItems [indexFoundAt].position;
+			} else {
+				mainCar.GetComponent<Transform> ().position = trackableItems [0].position;
 			}
 		}
+	}
+}
+public class TrackableItem {
+	public string name;
+	public Vector3 position;
+	public TrackableItem() {
 	}
 }
